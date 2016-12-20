56bd7662781a598e3e5f4b8095b4a99453720ad9
tkk <tkk@tkk-no-MacBook-Air.local>
2016-12-21 01:23:30 +0900

    Updated.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -115,4 +115,12 @@
 (require 'flycheck-google-cpplint)
 (flycheck-add-next-checker 'irony '(warning . c/c++-googlelint))
 
+(when (require 'rtags nil 'noerror)
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (when (rtags-is-indexed)
+                (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
+                (local-set-key (kbd "M-;") 'rtags-find-symbol)
+                (local-set-key (kbd "M-@") 'rtags-find-references)
+                (local-set-key (kbd "M-,") 'rtags-location-stack-back)))))
 (package-initialize)
